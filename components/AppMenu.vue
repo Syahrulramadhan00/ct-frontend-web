d55ea35d6de762fe340eb47178323908e27c22e4
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-07-13 12:40:14 +0700

    layouting side menu

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -1,5 +1,23 @@
 <template>
-  <div
-    class="flex-none bg-white shadow-2xl rounded-lg w-48 h-[34rem] mr-3 p-3"
-  ></div>
+  <div class="flex-none bg-white shadow-2xl rounded-lg w-48 h-[34rem] mr-3 p-3">
+    <!-- Brand Tag -->
+    <div class="flex flex-row justify-evenly items-center mb-5">
+      <div class="bg-black w-7 h-7"></div>
+      <p class="text-lg font-semibold">CahayaTeknik</p>
+    </div>
+    <!-- Menu -->
+    <div class="ml-2">
+      <AppMenuItem icon="pi-home" title="Dashboard" :isActive="true" />
+      <AppMenuItem icon="pi-box" title="Stok produk" />
+      <AppMenuItem icon="pi-shopping-bag" title="Pembelian" />
+      <Divider class="mb-8" />
+      <p class="font-medium text-slate-400">Penjualan</p>
+      <AppMenuItem icon="pi-receipt" title="Invoice" />
+      <AppMenuItem icon="pi-truck" title="Surat jalan" />
+      <AppMenuItem icon="pi-book" title="Tanda terima" class="mb-5" />
+      <Divider class="mb-8" />
+      <AppMenuItem icon="pi-address-book" title="Klien" />
+      <AppMenuItem icon="pi-chart-pie" title="Analitik" />
+    </div>
+  </div>
 </template>
---
+++ components/AppMenuItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex flex-row items-baseline mt-3">
+    <i
+      :class="[
+        'pi',
+        icon,
+        'mr-1',
+        'text-sm',
+        { 'text-purple-500': isActive, 'text-slate-400': !isActive },
+      ]"
+    ></i>
+    <p
+      :class="[
+        'text-sm',
+        'font-medium',
+        { 'text-purple-500': isActive, 'text-slate-400': !isActive },
+      ]"
+    >
+      {{ title }}
+    </p>
+  </div>
+</template>
+  
+  <script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+  
