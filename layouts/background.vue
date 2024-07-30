031bc21e3c393d86cb12e9c365ac46d4b90ea8b3
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-07-30 09:43:03 +0700

    add sign in page interface

---
+++ layouts/background.vue
@@ -0,0 +1,17 @@
+<script setup>
+import "primeicons/primeicons.css";
+</script>
+
+<template>
+  <div class="relative min-h-screen">
+    <div class="relative z-20 m-4 bg-none flex flex-col">
+      <slot />
+    </div>
+    <div
+      class="z-10 bg-sky-400 opacity-10 bg-fixed bg-cover bg-center h-screen blur-[128px] fixed top-0 left-0 right-0 bottom-0"
+    ></div>
+    <div
+      class="z-0 bg-[url('~/assets/img/main-background.png')] bg-fixed bg-cover bg-center h-screen blur-[80px] fixed top-0 left-0 right-0 bottom-0"
+    ></div>
+  </div>
+</template>
---
+++ pages/auth/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="bg-blur-lg backdrop-filter backdrop-blur-xl bg-white/65 rounded-2xl p-2 shadow-lg mt-44 min-w-96 lg:mx-44 md:mx-44 mx-4"
+  >
+    <div class="flex items-center justify-around flex-wrap">
+      <div class="md:mt-12 mx-2">
+        <p class="text-2xl font-light mb-2">Selamat datang 👋</p>
+        <p class="text-6xl font-bold">Sign in</p>
+      </div>
+      <div class="my-12">
+        <FloatLabel class="mt-4">
+          <InputText id="email" v-model="email" class="md:w-64 lg:w-[27rem]" />
+          <label for="email">Email</label>
+        </FloatLabel>
+        <div class="">
+          <FloatLabel class="mt-8">
+            <InputText
+              id="password"
+              v-model="password"
+              class="md:w-64 lg:w-[27rem]"
+            />
+            <label for="password">Password</label>
+          </FloatLabel>
+          <p class="mt-1 font-semibold text-blue-600 text-sm">Lupa password?</p>
+        </div>
+        <div class="flex flex-col items-center">
+          <div
+            class="main-container bg-purple-300 p-0 py-1 px-3 flex justify-evenly items-center mt-8 w-56 md:w-64 lg:w-[27rem] mb-2"
+          >
+            <p class="text-purple-800 font-semibold">Masuk</p>
+          </div>
+          <div class="flex justif">
+            <p class="text-sm mr-1">Tidak punya akun?</p>
+            <p class="font-semibold text-blue-600 text-sm">Daftar disini</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "background",
+});
+
+const email = ref(null);
+const password = ref(null);
+</script>
+
