38eb1d42b40ecb63860e86dbd9ab41acef9af74c
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-07-30 09:49:42 +0700

    add register page interface

---
+++ pages/auth/register.vue
@@ -0,0 +1,57 @@
+<template>
+  <div
+    class="bg-blur-lg backdrop-filter backdrop-blur-xl bg-white/65 rounded-2xl p-2 shadow-lg mt-44 min-w-96 lg:mx-44 md:mx-44 mx-4"
+  >
+    <div class="flex items-center justify-around flex-wrap">
+      <div class="md:mt-12 mx-2">
+        <p class="text-2xl font-light mb-2">Selamat datang 👋</p>
+        <p class="text-6xl font-bold">Register</p>
+      </div>
+      <div class="my-12">
+        <FloatLabel class="">
+          <InputText
+            id="username"
+            v-model="username"
+            class="md:w-64 lg:w-[27rem]"
+          />
+          <label for="username">Username</label>
+        </FloatLabel>
+        <FloatLabel class="mt-8">
+          <InputText id="email" v-model="email" class="md:w-64 lg:w-[27rem]" />
+          <label for="email">Email</label>
+        </FloatLabel>
+        <FloatLabel class="mt-8">
+          <InputText
+            id="password"
+            v-model="password"
+            class="md:w-64 lg:w-[27rem]"
+          />
+          <label for="password">Password</label>
+        </FloatLabel>
+        <div class="flex flex-col items-center">
+          <div
+            class="main-container bg-purple-300 p-0 py-1 px-3 flex justify-evenly items-center mt-8 w-56 md:w-64 lg:w-[27rem] mb-2"
+          >
+            <p class="text-purple-800 font-semibold">Daftar</p>
+          </div>
+          <div class="flex justif">
+            <p class="text-sm mr-1">Sudah punya akun?</p>
+            <p class="font-semibold text-blue-600 text-sm">Masuk disini</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+  
+  <script setup>
+definePageMeta({
+  layout: "background",
+});
+
+const email = ref(null);
+const password = ref(null);
+const username = ref(null);
+</script>
+  
+  
