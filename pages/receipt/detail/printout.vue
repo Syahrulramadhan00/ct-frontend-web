f01134e3f38b7203ef3b879fec6269043e63e0c2
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-09-15 22:14:34 +0700

    adjust printout invoices

--- pages/receipt/detail/printout.vue
+++ pages/receipt/detail/printout.vue
@@ -16,18 +16,18 @@ definePageMeta({
     </div>
 
     <!--  HEADER SECTION  -->
-    <div class="z-100 absolute ml-[120mm] mt-[20mm]">
+    <div class="z-100 absolute ml-[125mm] mt-[20mm]">
       <p class="text-sm">26 januari</p>
     </div>
-    <div class="z-100 absolute ml-[150mm] mt-[20mm]">
+    <div class="z-100 absolute ml-[155mm] mt-[20mm]">
       <p class="text-sm">04</p>
     </div>
-    <div class="z-100 absolute ml-[38mm] mt-[30mm]">
+    <div class="z-100 absolute ml-[38mm] mt-[33mm]">
       <p class="text-lg">177/X/SAM/SBY/22</p>
     </div>
-    <div class="z-100 absolute ml-[120mm] mt-[30mm]">
-      <p class="text-sm">PT Sinergi Makmur Sentosa</p>
-      <p class="text-sm">Raya Gding Pantai 31 sby</p>
+    <div class="z-100 absolute ml-[120mm] mt-[33mm]">
+      <p class="text-xs">PT Sinergi Makmur Sentosa</p>
+      <p class="text-xs">Raya Gding Pantai 31 sby</p>
     </div>
 
     <!--  ROW 1  -->
@@ -65,7 +65,7 @@ definePageMeta({
     </div>
 
     <!--  ROW 4  -->
-    <div class="z-100 absolute ml-[130mm] mt-[180mm]">
+    <div class="z-100 absolute ml-[130mm] mt-[176mm]">
       <p class="text-sm">Rp. 16,835,000</p>
     </div>
 
